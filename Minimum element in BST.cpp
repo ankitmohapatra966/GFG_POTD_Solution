ffdd2f7a86838df389265b91b6dcdf6f514b402a
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2024-03-30 08:53:22 +0530

    first commit

---
+++ Minimum element in BST.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:   
+    int minValue(Node* root) {
+        
+         if(!root){
+            return 1e9;
+        }
+        return min( root->data,min( minValue(root->left), minValue(root->right) ) ) ;
+    }
+
+};

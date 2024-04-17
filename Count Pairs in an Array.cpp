f8362dd2c31bfdc75ae34950b3e1e9386bb1d14e
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2024-04-17 09:29:09 +0530

    first commit

---
+++ Count Pairs in an Array.cpp
@@ -0,0 +1,24 @@
+class Solution{
+    public:
+    int countPairs(int arr[] , int n ) 
+    {
+        int prod[n];
+        int ans = 0;
+        int len = 0;
+        for(int i = 0; i < n; i++){
+            int curr = arr[i]*i;
+            int ind = upper_bound(prod, prod + len, curr) - prod;
+            ans += len - ind;
+            if(ind == len){
+                prod[len] = curr;
+            }else{
+                for(int j = len; j > ind; j--){
+                    prod[j] = prod[j - 1];
+                }
+                prod[ind] = curr;
+            }
+            len++;
+        }
+        return ans;
+    }
+};

ada6b51d7fc0d9ae6da3ea63290cf0dddf3d5135
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2024-04-20 14:33:24 +0530

    first commit

---
+++ Union of Two Sorted Arrays.cpp
@@ -0,0 +1,20 @@
+class Solution{
+    public:
+    vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        set <int>  s ;
+        for(int i = 0 ; i < n ; i++) {
+            s.insert(arr1[i])  ;
+        }
+        
+        for(int j = 0 ; j < m ; j++) {
+            s.insert(arr2[j])  ;
+        }
+        vector <int> ans ;
+        for( auto it : s) {
+            ans.push_back(it) ;
+        }
+        
+        return ans ;
+    }
+};

68c4f52dce51b0497f9fb4688255b37a076c6b17
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2024-04-28 09:28:54 +0530

    first commit

---
+++ Delete Middle of Linked List.cpp
@@ -0,0 +1,22 @@
+class Solution{
+    public:
+    Node* deleteMid(Node* head)
+    {
+        int cnt=0;
+        Node*head1=head;
+        while(head1!=NULL){
+            head1=head1->next;
+            cnt++;
+        }
+        if(cnt==1) return NULL;
+        int c=1;
+        Node*head2=head;
+        while(c<cnt/2){
+            head2=head2->next;
+            c++;
+        }
+        head2->next=(head2->next)->next;
+        head2=head2->next;
+        return head;
+    }
+};

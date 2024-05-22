9de8d349e382a4c6ab6fcb030ddc100b8dfca934
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2024-05-22 12:14:55 +0530

    first commit

--- Minimize Max Distance to Gas Station.cpp
+++ Minimize Max Distance to Gas Station.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> result;
-        vector<string> path;
-        backtrack(s, 0, path, result);
-        return result;
+    int countStations(double maxDist, vector<int> &arr) {
+        int cnt = 0, n = arr.size();
+        
+        for(int i = 0; i < n - 1; i++) {
+            double stationsInBetween = ((double) arr[i + 1] - arr[i]) / maxDist;
+            cnt += (int) stationsInBetween;
+        }
+        
+        return cnt;
     }
 
-private:
-    void backtrack(const string& s, int start, vector<string>& path, vector<vector<string>>& result) {
-        // If we've reached the end of the string, add the current partition to the result list
-        if (start == s.length()) {
-            result.push_back(path);
-            return;
+    double findSmallestMaxDist(vector<int> &arr, int k) {
+        int n = arr.size();
+        double maxi = INT_MIN;
+        
+        for(int i = 1; i < n; i++)
+            maxi = max(maxi, (double) arr[i] - arr[i - 1]);
+            
+        double low = 0, high = maxi, diff = 0.000001;
+        while(high - low > diff) {
+            double mid = low + (high - low) / 2;
+            
+            if(countStations(mid, arr) <= k)
+                high = mid;
+            else
+                low = mid;
         }
-        // Explore all possible partitions
-        for (int end = start + 1; end <= s.length(); ++end) {
-            // If the current substring is a palindrome, add it to the current path
-            if (isPalindrome(s, start, end - 1)) {
-                path.push_back(s.substr(start, end - start));
-                // Recur to find other partitions
-                backtrack(s, end, path, result);
-                // Backtrack to explore other partitions
-                path.pop_back();
-            }
-        }
-    }
-
-    bool isPalindrome(const string& s, int left, int right) {
-        // Check if the substring s[left:right+1] is a palindrome
-        while (left < right) {
-            if (s[left++] != s[right--]) {
-                return false;
-            }
-        }
-        return true;
+        
+        return high;
     }
 };

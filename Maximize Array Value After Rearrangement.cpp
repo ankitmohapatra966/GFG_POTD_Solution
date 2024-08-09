90436ac3a56681ee890fbed899a2d2c306003023
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2024-08-09 14:02:01 +0530

    first commit

---
+++ Maximize Array Value After Rearrangement.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int Maximize(vector<int> arr) {
+       sort(arr.begin(),arr.end());
+       long long ans=0;
+       long long mod=1e9+7;
+       for (int i=0;i<arr.size();i++)
+       {
+           ans=(ans%mod+((long long)arr[i]*i)%mod)%mod;
+       }
+       return ans;
+    }
+};

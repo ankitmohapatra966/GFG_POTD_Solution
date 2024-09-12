4ca65d1942e879756dc7231aa9019e583a8b9f71
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2024-09-12 18:31:32 +0530

    first commit

---
+++ Middle of a Linked List.cpp
@@ -0,0 +1,9 @@
+int getMiddle(Node* head) {
+        
+        Node* slow=head, *fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow->data;
+    }

93f122f399c54d01fc53942c06bf828f6c861e35
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2024-10-11 07:16:04 +0530

    first commit

---
+++ Reorganize The Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> rearrange(const vector<int>& arr) {
+        // Code here
+        int n = arr.size();
+        vector<int> ans(n, -1);
+        
+        for(int i=0; i<n; i++) {
+            if(arr[i] != -1) {
+                ans[arr[i]] = arr[i];
+            }
+        }
+        
+        return ans;
+    }
+};

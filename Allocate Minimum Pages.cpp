c87ed0bbf3ef731c0c81ebe3d1caa8fde4495767
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2024-12-18 09:02:12 +0530

    first commit

---
+++ Allocate Minimum Pages.cpp
@@ -0,0 +1,38 @@
+class Solution {
+  public:
+   bool isFeasible(vector<int>&arr, int k, int mid){
+        int req = 1, sum = 0;
+        
+        for(int i=0; i<arr.size(); i++){
+            if(sum + arr[i] > mid){
+                req++;
+                sum = arr[i];
+            }
+            else{
+                sum += arr[i];
+            }
+        }
+        return (req<=k);
+    }
+    int findPages(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        if(k > n) return -1;
+        
+        int sum = 0, mx = 0;
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+            mx = max(mx, arr[i]);
+        }
+        int low = mx, high = sum, res = 0;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(isFeasible(arr, k, mid)){
+                res = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return res;
+    }
+};

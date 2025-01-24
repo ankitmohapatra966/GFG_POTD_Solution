97fec5ed40c494d9afe6ee93cc07fcf141d037c2
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2025-01-24 08:32:51 +0530

    first commit

---
+++ Detect Loop in linked list.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    // Function to check if the linked list has a loop.
+    bool detectLoop(Node* head) {
+        // your code here
+        Node* slow=head;        
+        Node* fast=head;
+        
+        while(fast && fast->next){
+            fast=fast->next->next;
+            slow=slow->next;
+            
+            if(slow==fast)
+                return true;
+        }
+        
+        return false;
+    }
+};

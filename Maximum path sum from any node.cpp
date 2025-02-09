6fffdf6ed75306b3f6ff846a2037980ff8fd2d1c
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2025-02-09 09:12:01 +0530

    first commit

---
+++ Maximum path sum from any node.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int solve(Node* root,int &ans){
+        if(root==NULL) return 0;
+        int left=solve(root->left,ans);
+        int right=solve(root->right,ans);
+        int temp=max(0,max(left,right));
+        ans=max(ans,max(root->data+left+right ,root->data+temp));
+        
+        return root->data + temp;
+    }
+    int findMaxSum(Node *root) {
+        // code here
+        int ans=INT_MIN;
+        solve(root,ans);
+        return ans;
+    }
+};

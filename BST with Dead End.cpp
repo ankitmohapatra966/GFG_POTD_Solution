b5a05b26a4ec653f2835c377f170adc86edd889f
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2025-06-09 16:34:34 +0530

    first commit

---
+++ BST with Dead End.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+   bool solve(Node* node, int mini, int maxi){
+        if(!node) return false;
+        if(!node->left && !node->right){
+            return mini==maxi;
+        }
+        return solve(node->left, mini, node->data-1)||
+        solve(node->right, node->data+1, maxi);
+    }
+  
+    bool isDeadEnd(Node *root) {
+        // Code here
+        return solve(root, 1, 1e9);
+    }
+};

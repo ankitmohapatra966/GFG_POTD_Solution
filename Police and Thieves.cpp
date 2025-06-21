6ee87f86d2b010f7cbb88b90615a026e085b5eeb
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2025-06-21 13:36:57 +0530

    first commit

---
+++ Police and Thieves.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+    int catchThieves(vector<char> &arr, int k)
+    {
+        queue<pair<char, int>> q;
+        int ans = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            char curr = arr[i];
+            while (!q.empty() && i - q.front().second > k)
+                q.pop();
+
+            if (!q.empty() && q.front().first != curr)
+            {
+                ans++;
+                q.pop();
+            }
+            else
+            {
+                q.push({curr, i});
+            }
+        }
+        return ans;
+    }
+};

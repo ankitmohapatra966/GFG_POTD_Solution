9e764e0ad4e008e4d7d9f5fba9603cec8ebf9d8c
Ankit Mohapatra <126703297+ankitmohapatra966@users.noreply.github.com>
2025-07-26 18:34:52 +0530

    first commit

---
+++ Majority Element - More Than n divided by 3.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+     vector<int> findMajority(vector<int>& arr) {
+        map <int,int> mp;
+        vector<int> ans;
+        for(int e:arr)mp[e]++;
+        for(auto e:mp)if(e.second>arr.size()/3)ans.push_back(e.first);
+        return ans;
+    }
+};

6f8e3c83ebfd263c3e1c57f7eb543b0c495a2291
Ankit Mohapatra <126703297+ankitmohapatra966@users.noreply.github.com>
2025-08-30 19:39:27 +0530

    first commit

---
+++ The Celebrity_Problem.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int celebrity(vector<vector<int>>& mat) {
+        int n = mat.size();
+        for(int i = 0; i < n; ++i) {
+            int hor = 0, vert = 0;
+            for(int j = 0; j < n; ++j) {
+                if(i != j) {
+                    if(mat[i][j]) hor++;
+                    if(mat[j][i]) vert++;
+                }
+            }
+            if(hor == 0 && vert == n-1) return i;
+        }
+        return -1;
+    }
+};

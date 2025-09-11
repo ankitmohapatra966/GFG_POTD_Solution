9765275f20a870afc130932c7c4c9ed49d2b7094
Ankit Mohapatra <126703297+ankitmohapatra966@users.noreply.github.com>
2025-09-11 16:45:33 +0530

    first commit

---
+++ Minimum_Jumps.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int minJumps(vector<int>& arr) {
+        int n=arr.size();
+        int maxi=0;
+        int choice=0;
+        int ans=0;
+        for(int i=0;i<n-1;i++){
+            maxi=max(maxi,arr[i]+i);
+            if(i==choice){
+                choice=maxi;
+                ans++;
+            }
+        }
+        if(choice>=n-1){
+            return ans;
+        }
+        return -1;
+    }
+};

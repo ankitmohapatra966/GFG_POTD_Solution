8431d566285455111cd281d0158f2d82d96d3f3d
Ankit Mohapatra <126703297+ankitmohapatra966@users.noreply.github.com>
2025-10-17 20:22:35 +0530

    first commit

---
+++ BST to greater sum tree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int solve(Node* root, int sum){
+        if(!root)return 0;
+        int r = solve(root->right, sum);
+        int l = solve(root->left, root->data+sum+r);
+        int to_return = root->data+l+r;
+        root->data = r+sum;
+        return to_return;
+    }
+    void transformTree(Node *root) {
+        solve(root, 0);
+    }
+};

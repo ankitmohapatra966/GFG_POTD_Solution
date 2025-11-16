c6d30a8dbad094d05a67b05d1355104596512a95
Ankit Mohapatra <126703297+ankitmohapatra966@users.noreply.github.com>
2025-11-16 21:34:40 +0530

    first commit

---
+++ Longest Common Increasing Subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    virtual int LCIS(vector<int> &a, vector<int> &b) {
+      int n=a.size(), m=b.size();
+      vector<int> dp(m+1, 0);
+      
+      for(int &x: a){
+        int c=0;
+        for(int i=0;i<m;i++){
+          if(b[i]==x)
+            dp[i] = (dp[i], c+1);
+          if(b[i] < x)
+            c = max(dp[i], c);
+        }
+      }
+      
+      
+      int ans = 0;
+      for(int &x: dp)
+        ans = max(ans, x);
+      
+      return ans;
+    }
+};

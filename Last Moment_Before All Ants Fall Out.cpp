0e48fcbf243583820141e1ef2a50faabae85987d
Ankit Mohapatra <126703297+ankitmohapatra966@users.noreply.github.com>
2026-02-04 22:25:47 +0530

    first commit

---
+++ Last Moment_Before All Ants Fall Out.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        // code here
+        int maxtime = 0;
+        
+        for(int i=0;i<left.size();i++){
+            maxtime = max(maxtime,left[i]);
+        }
+        
+        for(int i=0;i<right.size();i++){
+            maxtime = max(maxtime,n-right[i]);
+        }
+        
+        return maxtime;
+    }
+};

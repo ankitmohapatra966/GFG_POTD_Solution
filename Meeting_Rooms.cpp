e646daf7629e7ce5c582866cecfa4fa139d882ba
Ankit Mohapatra <126703297+ankitmohapatra966@users.noreply.github.com>
2026-02-16 22:23:28 +0530

    first commit

---
+++ Meeting_Rooms.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    bool canAttend(vector<vector<int>> &arr) {
+        sort(arr.begin(), arr.end());
+        for(int i = 1;i<arr.size();i++){
+            if(arr[i-1][1]>arr[i][0])return false;
+        }
+        return true;
+    }
+};

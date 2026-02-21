93931be9aeccbec2b711ccfa507bc179a934630d
Ankit Mohapatra <126703297+ankitmohapatra966@users.noreply.github.com>
2026-02-21 11:46:15 +0530

    first commit

---
+++ Find_H-In_dex.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int hIndex(vector<int>& c) {
+        int n = c.size();
+        sort(c.begin(),c.end());
+        for(int i=0;i<n;i++){
+            if(n-i <= c[i])
+            return n-i;
+        }
+        return 0;
+    }
+};

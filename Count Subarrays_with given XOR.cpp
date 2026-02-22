4c7fd8c21ad50d0f5c42cc36bf931e9ef13da562
Ankit Mohapatra <126703297+ankitmohapatra966@users.noreply.github.com>
2026-02-22 20:18:02 +0530

    first commit

---
+++ Count Subarrays_with given XOR.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long subarrayXor(vector<int>& arr, int k) {
+        long ct = 0;
+        int c = 0;
+        unordered_map<int, int> mp;
+        mp.reserve(arr.size()); 
+        mp[0] = 1;              
+
+        for (const int& it : arr) {
+            c ^= it;
+            ct += mp[c ^ k];    
+            mp[c]++;
+        }
+
+        return ct;
+    }
+};

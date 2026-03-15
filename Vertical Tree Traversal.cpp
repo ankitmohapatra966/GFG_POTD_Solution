2680a55689ca9ad34a4033731079d35969a48d1d
Ankit Mohapatra <126703297+ankitmohapatra966@users.noreply.github.com>
2026-03-15 21:33:08 +0530

    first commit

---
+++ Vertical Tree Traversal.cpp
@@ -0,0 +1,38 @@
+using pri = pair<int, Node*>;
+using vi = vector<int>;
+
+class Solution {
+  public:
+    vector<vector<int>> verticalOrder(Node *root) {
+        // code here
+        vector<vi> ans;
+        
+        if (!root)
+            return ans;
+        
+        queue<pri> q;
+        map<int, vi> m;
+        
+        q.push({0, root});
+        
+        while (!q.empty()) {
+            auto d = q.front().first;
+            auto node = q.front().second;
+            
+            q.pop();
+            
+            m[d].push_back(node->data);
+            
+            if (node->left)
+                q.push({d -1 , node->left});
+            if (node->right)
+                q.push({d + 1, node->right});
+        }
+        
+        for (auto& [k, t] : m)
+            ans.push_back(t);
+        
+        return ans;
+    }
+};
+

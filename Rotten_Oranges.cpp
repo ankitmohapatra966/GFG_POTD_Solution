04cc7fae2fa88b2f1f8e02156f5419f25b131b3a
Ankit Mohapatra <126703297+ankitmohapatra966@users.noreply.github.com>
2026-03-22 21:21:00 +0530

    first commit

---
+++ Rotten_Oranges.cpp
@@ -0,0 +1,58 @@
+class Solution {
+  public:
+    int orangesRot(vector<vector<int>>& mat) {
+        int time=0,n=mat.size(),m=mat[0].size();
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==2)
+                    q.push({{i,j},0});
+            }
+        }
+        
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            time=it.second;
+ 
+            if(it.first.first>0){ 
+                if(mat[it.first.first-1][it.first.second]==1)
+                {
+                    q.push({{it.first.first-1,it.first.second},it.second+1});
+                    mat[it.first.first-1][it.first.second]=2;
+                }
+            }
+            if(it.first.first<n-1){ 
+                if(mat[it.first.first+1][it.first.second]==1)
+                {
+                    q.push({{it.first.first+1,it.first.second},it.second+1});
+                    mat[it.first.first+1][it.first.second]=2;
+                }
+            }
+            if(it.first.second>0){ 
+                if(mat[it.first.first][it.first.second-1]==1)
+                {
+                    q.push({{it.first.first,it.first.second-1},it.second+1});
+                    mat[it.first.first][it.first.second-1]=2;
+                }
+            }
+            if(it.first.second<m-1){
+                if(mat[it.first.first][it.first.second+1]==1)
+                {
+                    q.push({{it.first.first,it.first.second+1},it.second+1});
+                    mat[it.first.first][it.first.second+1]=2;
+                }
+            }
+            
+        }
+        
+        bool flag=true;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==1) flag=false;
+            }
+        }
+        
+        return flag?time:-1;
+    }
+};

26916eb80dc90a70031d9d7934647d357f85c4b9
Ankit Mohapatra <126703297+ankitmohapatra966@users.noreply.github.com>
2026-05-01 20:15:54 +0530

    first commit

---
+++ Kth Largest in a Stream.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    vector<int> kthLargest(vector<int>& arr, int k) {
+        // code here
+        int n = arr.size();
+        vector<int> ans(n, -1);
+        
+        priority_queue<int, vector<int>, greater<>> pq;
+        for(int i=0; i<n; i++){
+            pq.push(arr[i]);
+            if(pq.size() > k){
+                pq.pop();
+            }
+            if(pq.size() == k){
+                ans[i] = pq.top();
+            }
+        }
+        
+        return ans;
+    }
+};

f23b0c92eefab8ebad59ef678d3e3fcf9a49387a
Ankit Mohapatra <126703297+ankitmohapatra966@users.noreply.github.com>
2026-05-04 17:02:46 +0530

    first commit

---
+++ Palindrome Binary.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    bool isBinaryPalindrome(int n) {
+        if(n == 0) return true;
+        int x = log2(n) + 1;
+        
+        int i = 0, j = x-1;
+        
+        while(i < j){
+            int left  = (n >> i) & 1;
+            int right = (n >> j) & 1;
+
+            if(left != right) return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};

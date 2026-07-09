3e092c1c61bcb908de2d01341022b57f4857a961
Ankit Mohapatra <126703297+ankitmohapatra966@users.noreply.github.com>
2026-07-09 21:40:37 +0530

    first commit

---
+++ Count Pairs Divisible By K.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int countKdivPairs(vector<int>& arr, int k) {
+        // code here
+        vector<long long>freq(k,0);
+        
+        long long ans=0;
+        
+        
+        for(int x:arr)
+        {
+            int rem=x%k;
+            int need = (k-rem)%k;
+            ans+=freq[need];
+            freq[rem]++;
+        }
+        
+    return ans;
+        
+    }
+};
+
